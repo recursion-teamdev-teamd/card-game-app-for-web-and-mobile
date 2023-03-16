e871a3f0381e0ccae440b545c596b4bcd593d6be
kanta1207 <kan0712livepool@gmail.com>
2023-03-17 00:45:34 +0900

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'card-game-app',
+  webDir: 'out',
+  bundledWebRuntime: false,
+  "server" : {
+    "url" : "http://192.168.11.9:3000",
+    "cleartext" : true
+  }
+};
+
+export default config;
