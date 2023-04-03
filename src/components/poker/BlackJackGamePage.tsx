0cff2c9218988b606e711e40d5506b90868e7acb
yuki-akiba-jp <rabuafe@gmail.com>
2023-04-03 18:19:06 +0900

    create poker component

---
+++ src/components/poker/BlackJackGamePage.tsx
@@ -0,0 +1,48 @@
+import { PlayerField } from "./PlayerField";
+import { BlackJackGameButtons } from "./BlackJackGameButtons";
+
+import { FC } from "react";
+import React from "react";
+
+type Props = {
+  blackJackTable;
+  handleClickHitBtn;
+  handleClickStandBtn;
+  handleClickGameAgainBtn;
+};
+
+export const BlackJackGamePage: FC<Props> = (props) => {
+  const {
+    blackJackTable,
+    handleClickHitBtn,
+    handleClickStandBtn,
+    handleClickGameAgainBtn,
+  } = props;
+
+  const BlackJackGameButtonsProps = {
+    blackJackTable,
+    handleClickHitBtn,
+    handleClickStandBtn,
+    handleClickGameAgainBtn,
+  };
+
+  const houseProps = { player: blackJackTable.house };
+  const userProps = { player: blackJackTable.user };
+  return (
+    <>
+      <div>
+        <div className="flex  justify-center">
+          <div className="grid grid-rows-2 grid-flow-col gap-4">
+            <div className="flex bg-cyan-500 justify-center">
+              <PlayerField {...houseProps} />
+            </div>
+            <div className="flex  justify-center">
+              <PlayerField {...userProps} />
+            </div>
+          </div>
+        </div>
+        <BlackJackGameButtons {...BlackJackGameButtonsProps} />
+      </div>
+    </>
+  );
+};
---
+++ src/components/poker/BlackJackGameStartBtn.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FC } from "react";
+import { BasicButton } from "@/components/ui/atoms/buttons/BasicButton";
+
+type Props = {
+  handleClickGameStartBtn;
+};
+export const BlackJackGameStartBtn: FC<Props> = (props) => {
+  const { handleClickGameStartBtn } = props;
+
+  return (
+    <>
+      <BasicButton
+        buttonType="red"
+        mediaQueries="p-4 mr-3 h-5 "
+        onClick={handleClickGameStartBtn}
+      >
+        gamestart
+      </BasicButton>
+    </>
+  );
+};
